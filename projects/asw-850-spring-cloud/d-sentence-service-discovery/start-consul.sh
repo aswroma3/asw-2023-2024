2c339af36047ac7ce8d7340f31af9c8d24a3d11f
lucacabibbo <luca.cabibbo@uniroma3.it>
2023-11-22 12:21:50 +0100

    Aggiornato progetto asw-850-spring-cloud

--- projects/asw-850-spring-cloud/d-sentence-service-discovery/start-consul.sh
+++ projects/asw-850-spring-cloud/d-sentence-service-discovery/start-consul.sh
@@ -2,4 +2,4 @@
 
 echo Starting Consul in a Docker Container 
 
-docker run -d --hostname localhost --name asw-consul --publish 8500:8500 hashicorp/consul
+docker run -d --hostname localhost --name asw-consul --publish 8500:8500 docker.io/hashicorp/consul
